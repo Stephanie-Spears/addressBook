d158d4d6f8f3367738bcd58d074d45c1405d9b84
Stephanie Spears <stephanie.spears1@gmail.com>
2017-01-25 14:46:49 -0800

    add js changes

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,12 @@ function Contact(first, last, address, email, note){
   this.emailName = email;
   this.noteName = note;
 }
+//a protype method is a method meant to be called on a specific type of object. Ex: the Contact objects in our addresss book.
+//fullName() is a prototype method because it's specifically meant to return the full name of a Contact object.
+Contact.prototype.fullName = function(){
+  return this.firstName + " " + this.lastName;
+};
+
 
 $(document).ready(function(){
   $("form#newContactForm").submit(function(event){
@@ -13,11 +19,17 @@ $(document).ready(function(){
     var inputLastName = $("input#newLastName").val();
     var inputAddress = $("input#newAddress").val();
     var inputEmail = $("input#newEmail").val();
+    var inputNote = $("input#newNote").val();
+    $("input#newFirstName").val("");
+    $("input#newLastName").val("");
+    $("input#newAddress").val("");
+    $("input#newEmail").val("");
+    $("input#newNote").val("");
 
     // NOTE: creates a new Contact object by calling the Contract constructor and passing in the data. The variable newContact now refers to that newly created contact object, which knows it's "own" firstName and lastName
     var newContact = new Contact(inputFirstName, inputLastName, inputAddress, inputEmail);
 
-    $("ul#contactsList").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");//contains all the info of a contact but is just making a list of clickable items based on their first names
+    $("ul#contactsList").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");//contains all the info of a contact but is just making a list of clickable items based on their first names
 
     $(".contact").last().click(function(){ //for the contact that was "last" clicked, show their information
       $("#show-contact").show();
@@ -27,29 +39,17 @@ $(document).ready(function(){
       $(".address").text(newContact.addressName);
       $(".email").text(newContact.emailName);
     });
-
-    $("input#newFirstName").val("");
-    $("input#newLastName").val("");
-    $("input#newAddress").val("");
-    $("input#newEmail").val("");
-    $("input#newNote").val("");
-
-
-    $("#clearFormButton").click(function(event){
-      event.preventDefault();
-      $("input#newFirstName").val("");
-      $("input#newLastName").val("");
-      $("input#newAddress").val("");
-      $("input#newEmail").val("");
-      $("input#newNote").val("");
-      $(".contact").remove();
-      $(".firstName").remove();
-      $(".lastName").remove();
-      $(".address").remove();
-      $(".email").remove();
-      $(".note").remove();
-
-
-    });
+    // $("#clearFormButton").click(function(){
+    //
+    //   $("span .contact").removeClass();
+    //
+    //     $(".contact").remove();
+    //
+    //     $(".firstName").remove();
+    //     $(".lastName").remove();
+    //     $(".address").remove();
+    //     $(".email").remove();
+    //     $(".note").remove();
+    // });
   });
 });
